52bc97973f4458deb5869528bae6155aed7b2c44
myo2012 <42101273+myo2012@users.noreply.github.com>
2018-08-05 01:41:17 +0300

    Add files via upload

---
+++ BluetoothSMSGonder/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace BluetoothSMSGonder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numara = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mesaj = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.sp = new System.IO.Ports.SerialPort(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bağlantı Noktası:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(207, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Bağlan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numara
+            // 
+            this.numara.Location = new System.Drawing.Point(105, 48);
+            this.numara.Name = "numara";
+            this.numara.Size = new System.Drawing.Size(100, 20);
+            this.numara.TabIndex = 3;
+            this.numara.Text = "+90";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Numara :";
+            // 
+            // mesaj
+            // 
+            this.mesaj.Location = new System.Drawing.Point(105, 76);
+            this.mesaj.Name = "mesaj";
+            this.mesaj.Size = new System.Drawing.Size(220, 116);
+            this.mesaj.TabIndex = 5;
+            this.mesaj.Text = "";
+            this.mesaj.TextChanged += new System.EventHandler(this.mesaj_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mesaj :";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(103, 203);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Gönder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Bağlantı Durumu :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(102, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Bağlı Değil";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(278, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Bağlantıyı Kes";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(312, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 284);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.mesaj);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numara);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox numara;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox mesaj;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort sp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ BluetoothSMSGonder/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace BluetoothSMSGonder
+{
+    public partial class Form1 : Form
+    {
+
+    
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                if (sp.IsOpen)
+                {//Serial port açıksa aşağıdaki mesajı verdiriyoruz.
+                    MessageBox.Show("Şu an bir bağlantı noktasına zaten bağlı");
+                    label5.Text = sp.PortName + "  Baglandı";
+                    button1.Enabled = false;
+                    button3.Enabled = true;
+                }
+
+                else
+                {
+
+                    sp.PortName = textBox1.Text;
+                    sp.Open();
+                    //Serial portun port numarası olarak TextBox1 e girilen değeri atıyor ve bağlantı noktasını açıyoruz.
+                    if (sp.IsOpen)
+                    {//Bağlantı kurulunca forma aşağıdaki değişikleri yaptım.
+                        MessageBox.Show("Bağlantı Kuruldu");
+                        label5.Text = sp.PortName + "  Baglandı";
+                        
+                        button2.Enabled = true;
+                        button1.Enabled = false;
+                        button3.Enabled = true;
+
+                    }
+                    else
+                    {
+                        //Bağlantı kurulamadıysa aşağıdaki değişiklikleri yaptım.
+
+                        MessageBox.Show("Bağlantı Kurulamadı");
+                        label5.Text = "Bağlı Değil";
+                        button2.Enabled = false;
+                        button1.Enabled = true;
+                        button3.Enabled = false;
+                        textBox1.Text = "";
+
+                    }
+
+
+                }
+            }
+            catch(Exception hata)
+            {
+
+                //Bağlantı kurulamadıysa , COM portu bağlantı noktası bulunamadıysa oluacak her türlü hatayı try-catch ile yakalatak
+                //Oluan hata mesajını verdiriyorum.
+                MessageBox.Show(hata.Message);
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sp.Close();
+            button1.Enabled = true;
+            button3.Enabled = false;
+            MessageBox.Show("Bağlantı Kesildi.");
+            label5.Text = "Bağlı Değil";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sp.WriteLine("AT" + "\r");
+            //Telefona AT komutları kullanacağımzı bildiriyoruz.
+            System.Threading.Thread.Sleep(250);
+            sp.WriteLine("AT+CMGF=1" + "\r");
+            //Yeni Mesaj göndermek istediğimizi bildiriyoruz.
+            System.Threading.Thread.Sleep(250);
+            sp.WriteLine("AT+CMGS=\"" + numara.Text + "\"" + "\r\n");
+            //Mesajın hangi numaraya gideceğini bildiriyoruz.
+            System.Threading.Thread.Sleep(250);
+            sp.WriteLine(turkceKarakterTemizle(mesaj.Text) + Char.ConvertFromUtf32(26) + "\r");
+            //Türkçe karakter desteği olmadğı için Türkçe karakterleri temizliyoruz.
+            //Mesajın bittiğine dair komutu CTRL+Z komutunun ASCII kod karşılığı ile bildiriyoru.
+            System.Threading.Thread.Sleep(3000);
+            sp.WriteLine("AT+CMSS=1" + "\r");
+            //Mesajı gönder diyoruz.
+            MessageBox.Show("Mesaj Gönderildi");
+            temizle();
+        }
+
+        private void temizle()
+        {
+            numara.Text = "+90";
+            mesaj.Text = "";
+        }
+
+        private void mesaj_TextChanged(object sender, EventArgs e)
+        {
+            //Mesajın kaç karakterden oluştuğunu yazdırıyorum.
+            label6.Text = mesaj.Text.Length.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string turkceKarakterTemizle(string mesaj)
+        {
+            //Mesaj olarak girilen içeriğin Türkçe karakterlerini temizliyorum.
+            string geriDonecekDeger = mesaj.ToUpper().Replace('Ç','C').Replace('Ğ','G').Replace('İ','I').Replace ('Ö','O').Replace('Ş','S').Replace ('Ü','U');
+            return geriDonecekDeger;
+
+        
+        }
+
+
+    }
+}
